f798587c32d1556bd6a398165b48425b5c0ce177
bantypatel09 <bantypatel8242217@gmail.com>
2020-12-02 11:36:29 -0500

    more touches to search

--- app/src/main/java/com/example/androidphotos/Search.java
+++ app/src/main/java/com/example/androidphotos/Search.java
@@ -96,9 +96,15 @@ public class Search extends AppCompatActivity {
             //loop through photos and see if photo has both tags if so add to listview
             int count = 0;
             for(Photo p: photos){
-                if(p.getTags().contains(totalTag1) && p.getTags().contains(totalTag2)){
-                    count++;
-                    //add to listview
+                for(String p1: p.getTags()){
+                    if(p1.contains(totalTag1)){
+                        for(String p2: p.getTags()){
+                            if(p2.contains(totalTag2)){
+                                count++;
+                                //add to listview
+                            }
+                        }
+                    }
                 }
             }
             if(count == 0){
@@ -114,10 +120,118 @@ public class Search extends AppCompatActivity {
         }
         //OR CONJUNCTION
         else if (conjunctionType.equals("OR")){
-
+            String firstTag = tag1.getText().toString().trim();
+            String secondTag = tag2.getText().toString().trim();
+            if(firstTag.equals("") || firstTag == null || secondTag.equals("") || secondTag == null){
+                //show pop-up error (at least one tag is required)
+                Bundle bundle = new Bundle();
+                bundle.putString(PopupDialog.MESSAGE_KEY, "Tag fields are empty, fill in at least one tag field");
+                DialogFragment newFragment = new PopupDialog();
+                newFragment.setArguments(bundle);
+                newFragment.show(getSupportFragmentManager(),"badfields");
+                return;
+            }
+            String firstTagType = tagType1.getSelectedItem().toString();
+            String secondTagType = tagType2.getSelectedItem().toString();
+            //if only first tag is filled
+            if(!firstTag.equals("") && (secondTag.equals("") || secondTag == null)){
+                if(firstTagType.equals("") || firstTagType == null){
+                    //show pop-up error (tag type 1 is required)
+                    Bundle bundle = new Bundle();
+                    bundle.putString(PopupDialog.MESSAGE_KEY, "Please select a tag type for the first tag");
+                    DialogFragment newFragment = new PopupDialog();
+                    newFragment.setArguments(bundle);
+                    newFragment.show(getSupportFragmentManager(),"badfields");
+                    return;
+                }
+                String totalTag1 = firstTagType + ": " + firstTag;
+                //loop through photos and see if photo has both tags if so add to listview
+                int count = 0;
+                for(Photo p: photos){
+                    for(String t: p.getTags()){
+                        if(t.contains(totalTag1)){
+                            //add to list view
+                            count++;
+                        }
+                    }
+                }
+                if(count == 0){
+                    //show pop-up error (no photo with provided tags)
+                    Bundle bundle = new Bundle();
+                    bundle.putString(PopupDialog.MESSAGE_KEY, "There are no photos with the provided tags");
+                    DialogFragment newFragment = new PopupDialog();
+                    newFragment.setArguments(bundle);
+                    newFragment.show(getSupportFragmentManager(),"badfields");
+                    return;
+                }
+            }
+            //if only second tag is filled
+            else if(!secondTag.equals("") && (firstTag.equals("") || firstTag == null)){
+                if(secondTagType.equals("") || secondTagType == null){
+                    //show pop-up error (tag type 2 is required required)
+                    Bundle bundle = new Bundle();
+                    bundle.putString(PopupDialog.MESSAGE_KEY, "Please select a tag type for the second tag");
+                    DialogFragment newFragment = new PopupDialog();
+                    newFragment.setArguments(bundle);
+                    newFragment.show(getSupportFragmentManager(),"badfields");
+                    return;
+                }
+                String totalTag2 = secondTagType + ": " + secondTag;
+                //loop through photos and see if photo has both tags if so add to listview
+                int count = 0;
+                for(Photo p: photos){
+                    for(String t: p.getTags()){
+                        if(t.contains(totalTag2)){
+                            //add to list view
+                            count++;
+                        }
+                    }
+                }
+                if(count == 0){
+                    //show pop-up error (no photo with provided tags)
+                    Bundle bundle = new Bundle();
+                    bundle.putString(PopupDialog.MESSAGE_KEY, "There are no photos with the provided tags");
+                    DialogFragment newFragment = new PopupDialog();
+                    newFragment.setArguments(bundle);
+                    newFragment.show(getSupportFragmentManager(),"badfields");
+                    return;
+                }
+            }
+            //if both tags are filled
+            else if(!firstTag.equals("") && !secondTag.equals("")){
+                if(firstTagType.equals("") || firstTagType == null || secondTagType.equals("") || secondTagType == null){
+                    //show pop-up error (both tag types are required required)
+                    Bundle bundle = new Bundle();
+                    bundle.putString(PopupDialog.MESSAGE_KEY, "Please select a tag type for both tags");
+                    DialogFragment newFragment = new PopupDialog();
+                    newFragment.setArguments(bundle);
+                    newFragment.show(getSupportFragmentManager(),"badfields");
+                    return;
+                }
+                String totalTag1 = firstTagType + ": " + firstTag;
+                String totalTag2 = secondTagType + ": " + secondTag;
+                //loop through photos and see if photo has one of the tags if so add to listview
+                int count = 0;
+                for(Photo p: photos){
+                    for(String p1: p.getTags()){
+                        if(p1.contains(totalTag1) || p1.contains(totalTag2)){
+                            count++;
+                            //add to listview
+                        }
+                    }
+                }
+                if(count == 0){
+                    //show pop-up error (no photo with provided tags)
+                    Bundle bundle = new Bundle();
+                    bundle.putString(PopupDialog.MESSAGE_KEY, "There are no photos with the provided tags");
+                    DialogFragment newFragment = new PopupDialog();
+                    newFragment.setArguments(bundle);
+                    newFragment.show(getSupportFragmentManager(),"badfields");
+                    return;
+                }
+            }
         }
         else{
-            //display error to choose conjunction type
             //show pop-up error (conjunction is required)
             Bundle bundle = new Bundle();
             bundle.putString(PopupDialog.MESSAGE_KEY, "Please choose a conjunction");
